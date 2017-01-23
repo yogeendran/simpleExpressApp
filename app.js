8f02fe16f4519a5bbe622880f3a68f31baa24241
yogeendran <yogeendran.jayatheerthan@wipro.com>
2017-01-23 11:18:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express= require('express');
 var app=express();
 var one=require('./one.js');
-var three=require('./one.js');
+var three=require('./three.js');
 var port=8000;
 var bodyParser = require('body-parser');
 
