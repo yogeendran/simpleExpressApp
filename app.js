b078e09e405f9da3177ca123ea63cb3123e00847
yogeendran <yogeendran.jayatheerthan@wipro.com>
2017-01-23 11:17:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express= require('express');
 var app=express();
-var four=require('./one.js');
+var one=require('./one.js');
+var three=require('./one.js');
 var port=8000;
 var bodyParser = require('body-parser');
 
@@ -10,7 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //To parse json data
 app.use(bodyParser.json())
 app.get('/one', function(req,res){
-	four.finddata(req,res);
+	one.finddata(req,res);
+app.post('/three', function(req,res){
+	three.finddata(req,res);
 });
 app.listen(port);
 console.log("server started");
